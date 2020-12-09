5e42550cecfc9d33b3ea96748f212f5b8cc33e98
RujoiRazvan <57172561+RujoiRazvan@users.noreply.github.com>
2020-12-09 17:57:46 +0200

    Add files via upload

--- Proiect_Info_Aplicata_v1.ino
+++ Proiect_Info_Aplicata_v1.ino
@@ -1,21 +1,21 @@
 //Proiect InfoAplicata II
-//Sistem automat de udat florile (actionat prin wifi sau de la buton)
+//Sistem automat de udat plante (actionat prin wifi sau de la buton)
 //Cabluri Rosii - 5V/3V. Cabluri Negre (Gri) - Ground.
 
-#include <Adafruit_GFX.h>
 #include <Adafruit_PCD8544.h>
 #include <dht.h>
 
 const int buton = 5;
 const int ledPin =  7;
-int pompaApa = 4;
+const int pompaApa = 4;
 int buzzer = 6;
 int senzorPamant = A0;
 int valoareButon = 0;
+
 #define tempSenzorPin 2
 dht DHT;
 
-Adafruit_PCD8544 display = Adafruit_PCD8544(12, 11, 10, 9, 8);
+Adafruit_PCD8544 display = Adafruit_PCD8544(12, 11, 10, 9, 8); //initializarea pinilor de pe lcd
 
 void setup() {
   pinMode(pompaApa, OUTPUT);
@@ -23,25 +23,25 @@ void setup() {
   pinMode(buzzer, OUTPUT);
   pinMode(ledPin, OUTPUT);
   pinMode(buton, INPUT);
-
-  display.setRotation(2);
-  Serial.begin(9600);
+ 
+  Serial.begin(9600); //initializez conexiunea seriala la 9600 biti pe secunda
+  
   display.begin();
+  display.setRotation(2);
   display.setContrast(10);
   display.clearDisplay();
   display.setTextSize(1);
   display.setTextColor(BLACK);
+  
   display.setCursor(0, 6);
   display.println("Rujoi Razvan");
 
   display.setTextColor(WHITE, BLACK);
-
   display.println("Proiect IA II");
 
   display.setTextSize(1);
   display.setTextColor(BLACK);
-  display.println();
-  display.print("Sistem automat");
+  display.println("Sistem automat");
   display.println("de udat plante");
   display.display();
   delay(2000);
@@ -50,9 +50,12 @@ void setup() {
 void loop() {
   valoareButon = digitalRead(buton);
   int valoareSenzorPamant = analogRead(senzorPamant);
+  
+  Serial.println("Valoare senzor pamant: ");
   Serial.println(valoareSenzorPamant);
+  
   DHT.read11(tempSenzorPin);
-  float temperaturaCamera = DHT.temperature;
+  double temperaturaCamera = DHT.temperature;
   display.display();
   display.clearDisplay();
   display.setContrast(40);
@@ -65,12 +68,12 @@ void loop() {
   display.print("C");
   display.print("Umiditate ");
   display.print("sol: ");
-  display.print(valoareSenzorPamant);
+  display.print(valoareSenzorPamant / 100);
   display.print("%");
   digitalWrite(ledPin, LOW);
   noTone(buzzer);
 
-  if (valoareSenzorPamant <= 1 || valoareButon == HIGH) {
+  if (valoareSenzorPamant >= 900 || valoareButon == HIGH) {
     display.display();
     display.clearDisplay();
     display.setTextSize(1);
@@ -81,20 +84,25 @@ void loop() {
     display.display();
     digitalWrite(ledPin, HIGH);
     digitalWrite(pompaApa, HIGH);
-    tone(buzzer, 2000);
+    tone(buzzer, 3000);
     delay(5000);
     digitalWrite(ledPin, LOW);
     digitalWrite(pompaApa, LOW);
     noTone(buzzer);
-    display.clearDisplay();
-    display.display();
     DHT.read11(tempSenzorPin);
     temperaturaCamera = DHT.temperature;
+    display.clearDisplay();
+    display.display();
+
     display.setCursor(0, 0);
     display.println("Temperatura ");
     display.print("camera: ");
     display.print(temperaturaCamera);
     display.print("C");
+    display.print("Umiditate ");
+    display.print("sol: ");
+    display.print(valoareSenzorPamant);
+    display.print("%");
   }
 
   delay(1000);
